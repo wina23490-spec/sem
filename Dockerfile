a708fa0b04bfecd49f73dc76b634b548985294cb
Win Aung <wina23490@gmail.com>
2026-09-17 11:18:50 +0630

    test 1

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,4 @@
-FROM eclipse-temurin:26
-
-COPY ./target/classes/com /tmp/com
-
+FROM eclipse-temurin:25
+COPY ./target/seMethod-1.0-SNAPSHOT-jar-with-dependencies.jar /tmp
 WORKDIR /tmp
-
-ENTRYPOINT ["java", "com.napier.sem.App"]
+ENTRYPOINT ["java", "-jar", "seMethod-1.0-SNAPSHOT-jar-with-dependencies.jar"]
--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -1,7 +1,34 @@
 package com.napier.sem;
 
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.MongoCollection;
+import org.bson.Document;
+
 public class App {
+
     public static void main(String[] args) {
-        System.out.println("Welcome From IMC");
+
+        MongoClient mongoClient = new MongoClient("localhost", 27000);
+
+        MongoDatabase database = mongoClient.getDatabase("mydb");
+
+        MongoCollection<Document> collection =
+                database.getCollection("test");
+
+        Document doc = new Document("name", "Kevin Sim")
+                .append("class", "DevOps")
+                .append("year", "2024")
+                .append("result",
+                        new Document("CW", 95)
+                                .append("EX", 85));
+
+        collection.insertOne(doc);
+
+        Document myDoc = collection.find().first();
+
+        System.out.println(myDoc.toJson());
+
+        mongoClient.close();
     }
 }
