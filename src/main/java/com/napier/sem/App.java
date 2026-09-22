7885048e560e63a94dbcfb8017c91195621b875e
Win Aung <wina23490@gmail.com>
2026-09-22 16:15:18 +0630

    Pre

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -9,7 +9,7 @@ public class App {
 
     public static void main(String[] args) {
 
-        MongoClient mongoClient = new MongoClient("localhost", 27000);
+        MongoClient mongoClient = new MongoClient("mongo-dbserver", 27017);
 
         MongoDatabase database = mongoClient.getDatabase("mydb");
 
